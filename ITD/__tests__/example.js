6adbe005a342d8e6ff42bceda53038213a8ce74d
Andrea Franchini <hello@andreafranchini.com>
2021-01-15 20:40:55 +0100

    Add Workspaces Add Jest Config Add @types pkgs Remove old .vscode files

---
+++ ITD/__tests__/example.js
@@ -0,0 +1,5 @@
+// EXAMPLE TEST
+test("two plus two is four", () => {
+	console.log({ name: "Andrea" })
+	expect(2 + 2).toBe(4)
+})
--- ITD/src/server/components/QueryManager/index.js
+++ ITD/src/server/components/QueryManager/index.js
@@ -11,62 +11,85 @@ const mysqlConection = mysql.createConnection({
 	user: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
-});
-
-mysqlConection.connect(function (err) {
-    if (err) throw err
 })
 
-exports.close = () => mysqlConection.end((err) => { if (err) throw err })
-  
+mysqlConection.connect(function (err) {
+	if (err) throw err
+})
+
+exports.close = () =>
+	mysqlConection.end((err) => {
+		if (err) throw err
+	})
+
 exports.executeAndRollback = (callback) => {
-    mysqlConection.beginTransaction((err) => {
-        if (err) throw err
+	mysqlConection.beginTransaction((err) => {
+		if (err) throw err
 
-        callback()
+		callback()
 
-        mysqlConnection.rollback();
-    })
+		mysqlConnection.rollback()
+	})
 }
 
 exports.checkIfPhoneNumberIsPresent = (phoneNum) => {
-    var isPresent
+	var isPresent
 
-    mysqlConection.query("select count(*) from user where id = ?", phoneNum, (err, result) => {
-        if (err) throw err
-        isPresent = result
-    })
+	mysqlConection.query(
+		"select count(*) from user where id = ?",
+		phoneNum,
+		(err, result) => {
+			if (err) throw err
+			isPresent = result
+		}
+	)
 
-    return isPresent
+	return isPresent
 }
 
 exports.createUser = (phoneNum, name, surname) => {
-    mysqlConection.query("insert into user (id, name, surname) values (?,?,?)", [phoneNum, name, surname], (err) => {
-        if (err) throw err
-    })
+	mysqlConection.query(
+		"insert into user (id, name, surname) values (?,?,?)",
+		[phoneNum, name, surname],
+		(err) => {
+			if (err) throw err
+		}
+	)
 }
 
 exports.createUserToken = (phoneNum) => {
-    mysqlConection.query("remove from token where user_id = ?", phoneNum, (err) => {
-        if (err) throw err
-    })
+	mysqlConection.query(
+		"remove from token where user_id = ?",
+		phoneNum,
+		(err) => {
+			if (err) throw err
+		}
+	)
 
-    const userToken = uuid.v4()
+	const userToken = uuid.v4()
 
-    mysqlConection.query("insert into token (user_id, token, timestamp) values (?, ?, 2038-01-19 03:14:07)", [phoneNum, userToken], (err) => {
-        if (err) throw err
-    })
+	mysqlConection.query(
+		"insert into token (user_id, token, timestamp) values (?, ?, 2038-01-19 03:14:07)",
+		[phoneNum, userToken],
+		(err) => {
+			if (err) throw err
+		}
+	)
 
-    return userToken
+	return userToken
 }
 
 exports.validateToken = (token) => {
-    var userId
+	var userId
 
-    mysqlConection.query("select id from token where token = ?", token, (err, result) => {
-        if (err) throw err
-        userId = result
-    })
+	mysqlConection.query(
+		"select id from token where token = ?",
+		token,
+		(err, result) => {
+			if (err) throw err
+			userId = result
+		}
+	)
 
-    return userId
+	return userId
 }
