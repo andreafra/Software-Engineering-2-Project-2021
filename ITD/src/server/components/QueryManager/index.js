83001082b675da06b396fae0074cbb2960428fb9
luigifusco <luigifusco98@gmail.com>
2021-01-15 20:32:57 +0100

    rewrite database connection code using promise-mysql

--- ITD/src/server/components/QueryManager/index.js
+++ ITD/src/server/components/QueryManager/index.js
@@ -1,72 +1,56 @@
 const dotenv = require("dotenv")
 dotenv.config()
 
-const mysql = require("mysql")
+const mysql = require("promise-mysql")
 const fs = require("fs")
 const uuid = require("uuid")
 
-const mysqlConection = mysql.createConnection({
-	multipleStatements: true,
-	host: process.env.DB_ADDRESS,
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-});
 
-mysqlConection.connect(function (err) {
-    if (err) throw err
-})
+var mysqlConnection
 
-exports.close = () => mysqlConection.end((err) => { if (err) throw err })
+exports.getQueryInterface = async () => {
+    if (mysqlConnection === undefined) {
+        mysqlConnection = await mysql.createConnection({
+            multipleStatements: true,
+            host: process.env.DB_ADDRESS,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        })
+    }
+
+    return {
+        executeAndRollback: async (callback) => {
+            await mysqlConnection.beginTransaction()
+            await callback()
+            await mysqlConnection.rollback()
+        },
+        
+        checkIfPhoneNumberIsPresent: async (phoneNum) => {
+            return (await mysqlConnection.query("select count(*) as count from user where id = ?", phoneNum))[0].count === 1
+        },
+        
+        createUser: async (phoneNum, name, surname) => {
+            return await mysqlConnection.query("insert into user (id, name, surname) values (?,?,?)", [phoneNum, name, surname])
+        },
+        
+        createUserToken: async (phoneNum) => {
+            await mysqlConnection.query("remove from token where user_id = ?", phoneNum)
+        
+            const userToken = uuid.v4()
+        
+            await mysqlConnection.query("insert into token (user_id, token, timestamp) values (?, ?, 2038-01-19 03:14:07)", [phoneNum, userToken])
+        
+            return userToken
+        },
+        
+        validateToken: async (token) => {
+            return await mysqlConnection.query("select id from token where token = ?", token)
+        },
+
+        globalEnd: async () => {
+            await mysqlConnection.end()
+        }
+    }
+}
   
-exports.executeAndRollback = (callback) => {
-    mysqlConection.beginTransaction((err) => {
-        if (err) throw err
-
-        callback()
-
-        mysqlConnection.rollback();
-    })
-}
-
-exports.checkIfPhoneNumberIsPresent = (phoneNum) => {
-    var isPresent
-
-    mysqlConection.query("select count(*) from user where id = ?", phoneNum, (err, result) => {
-        if (err) throw err
-        isPresent = result
-    })
-
-    return isPresent
-}
-
-exports.createUser = (phoneNum, name, surname) => {
-    mysqlConection.query("insert into user (id, name, surname) values (?,?,?)", [phoneNum, name, surname], (err) => {
-        if (err) throw err
-    })
-}
-
-exports.createUserToken = (phoneNum) => {
-    mysqlConection.query("remove from token where user_id = ?", phoneNum, (err) => {
-        if (err) throw err
-    })
-
-    const userToken = uuid.v4()
-
-    mysqlConection.query("insert into token (user_id, token, timestamp) values (?, ?, 2038-01-19 03:14:07)", [phoneNum, userToken], (err) => {
-        if (err) throw err
-    })
-
-    return userToken
-}
-
-exports.validateToken = (token) => {
-    var userId
-
-    mysqlConection.query("select id from token where token = ?", token, (err, result) => {
-        if (err) throw err
-        userId = result
-    })
-
-    return userId
-}
