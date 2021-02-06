8216c314eeaa6769d7d2b6c7167175960588517d
ian-ofgod <ian.didio@mail.polimi.it>
2021-02-06 18:58:41 +0100

    Added DEMO db schema and tuples

--- ITD/database/init/db_clup.sql
+++ ITD/database/init/db_clup.sql
@@ -1,52 +1,29 @@
--- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
---
--- Host: localhost    Database: db_clup
--- ------------------------------------------------------
--- Server version	8.0.21
+-- Adminer 4.7.8 MySQL dump
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
---
--- Table structure for table `admin`
---
+SET NAMES utf8mb4;
+
+DROP DATABASE IF EXISTS `db_clup`;
+CREATE DATABASE `db_clup` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_clup`;
 
 DROP TABLE IF EXISTS `admin`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `admin`
---
-
-LOCK TABLES `admin` WRITE;
-/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
-/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `favorites`
---
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1,	'admin',	'password');
 
 DROP TABLE IF EXISTS `favorites`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `favorites` (
   `user_id` varchar(20) NOT NULL,
   `store_id` int NOT NULL,
@@ -55,24 +32,9 @@ CREATE TABLE `favorites` (
   CONSTRAINT `fav_store` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`),
   CONSTRAINT `fav_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `favorites`
---
-
-LOCK TABLES `favorites` WRITE;
-/*!40000 ALTER TABLE `favorites` DISABLE KEYS */;
-/*!40000 ALTER TABLE `favorites` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `free_timeslot_notification`
---
 
 DROP TABLE IF EXISTS `free_timeslot_notification`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `free_timeslot_notification` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` varchar(20) NOT NULL,
@@ -84,24 +46,9 @@ CREATE TABLE `free_timeslot_notification` (
   KEY `notification_user_idx` (`user_id`),
   CONSTRAINT `notification_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `free_timeslot_notification`
---
-
-LOCK TABLES `free_timeslot_notification` WRITE;
-/*!40000 ALTER TABLE `free_timeslot_notification` DISABLE KEYS */;
-/*!40000 ALTER TABLE `free_timeslot_notification` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `managed_store`
---
 
 DROP TABLE IF EXISTS `managed_store`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `managed_store` (
   `store_id` int NOT NULL,
   `admin_id` int NOT NULL,
@@ -110,24 +57,13 @@ CREATE TABLE `managed_store` (
   CONSTRAINT `ms_admin` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`),
   CONSTRAINT `ms_store` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `managed_store`
---
-
-LOCK TABLES `managed_store` WRITE;
-/*!40000 ALTER TABLE `managed_store` DISABLE KEYS */;
-/*!40000 ALTER TABLE `managed_store` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reservation`
---
+INSERT INTO `managed_store` (`store_id`, `admin_id`) VALUES
+(1,	1),
+(2,	1),
+(3,	1);
 
 DROP TABLE IF EXISTS `reservation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reservation` (
   `id` int NOT NULL AUTO_INCREMENT,
   `weekday` int NOT NULL,
@@ -139,25 +75,71 @@ CREATE TABLE `reservation` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `store_id_idx` (`store_id`),
   CONSTRAINT `` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `reservation`
---
-
-LOCK TABLES `reservation` WRITE;
-/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `store`
---
+INSERT INTO `reservation` (`id`, `weekday`, `start_time`, `max_people_allowed`, `is_active`, `store_id`) VALUES
+(172,	1,	'08:00:00',	10,	1,	1),
+(173,	1,	'10:00:00',	10,	1,	1),
+(174,	1,	'13:00:00',	10,	1,	1),
+(175,	1,	'16:00:00',	10,	1,	1),
+(176,	1,	'17:00:00',	10,	1,	1),
+(177,	1,	'18:00:00',	10,	1,	1),
+(178,	2,	'08:00:00',	10,	1,	1),
+(179,	2,	'10:00:00',	10,	1,	1),
+(180,	2,	'16:00:00',	10,	1,	1),
+(181,	2,	'17:00:00',	10,	1,	1),
+(182,	2,	'18:00:00',	10,	1,	1),
+(183,	3,	'08:00:00',	10,	1,	1),
+(184,	3,	'10:00:00',	10,	1,	1),
+(185,	3,	'16:00:00',	10,	1,	1),
+(186,	3,	'18:00:00',	10,	1,	1),
+(187,	4,	'08:00:00',	10,	1,	1),
+(188,	4,	'10:00:00',	10,	1,	1),
+(189,	4,	'16:00:00',	10,	1,	1),
+(190,	4,	'17:00:00',	10,	1,	1),
+(191,	4,	'18:00:00',	10,	1,	1),
+(192,	1,	'08:00:00',	10,	1,	2),
+(193,	1,	'10:00:00',	10,	1,	2),
+(194,	1,	'13:00:00',	10,	1,	2),
+(195,	1,	'16:00:00',	10,	1,	2),
+(196,	1,	'17:00:00',	10,	1,	2),
+(197,	1,	'18:00:00',	10,	1,	2),
+(198,	2,	'08:00:00',	10,	1,	2),
+(199,	2,	'10:00:00',	10,	1,	2),
+(200,	2,	'16:00:00',	10,	1,	2),
+(201,	2,	'17:00:00',	10,	1,	2),
+(202,	2,	'18:00:00',	10,	1,	2),
+(203,	3,	'08:00:00',	10,	1,	2),
+(204,	3,	'10:00:00',	10,	1,	2),
+(205,	3,	'16:00:00',	10,	1,	2),
+(206,	3,	'18:00:00',	10,	1,	2),
+(207,	4,	'08:00:00',	10,	1,	2),
+(208,	4,	'10:00:00',	10,	1,	2),
+(209,	4,	'16:00:00',	10,	1,	2),
+(210,	4,	'17:00:00',	10,	1,	2),
+(211,	4,	'18:00:00',	10,	1,	2),
+(212,	1,	'08:00:00',	10,	1,	3),
+(213,	1,	'10:00:00',	10,	1,	3),
+(214,	1,	'13:00:00',	10,	1,	3),
+(215,	1,	'16:00:00',	10,	1,	3),
+(216,	1,	'17:00:00',	10,	1,	3),
+(217,	1,	'18:00:00',	10,	1,	3),
+(218,	2,	'08:00:00',	10,	1,	3),
+(219,	2,	'10:00:00',	10,	1,	3),
+(220,	2,	'16:00:00',	10,	1,	3),
+(221,	2,	'17:00:00',	10,	1,	3),
+(222,	2,	'18:00:00',	10,	1,	3),
+(223,	3,	'08:00:00',	10,	1,	3),
+(224,	3,	'10:00:00',	10,	1,	3),
+(225,	3,	'16:00:00',	10,	1,	3),
+(226,	3,	'18:00:00',	10,	1,	3),
+(227,	4,	'08:00:00',	10,	1,	3),
+(228,	4,	'10:00:00',	10,	1,	3),
+(229,	4,	'16:00:00',	10,	1,	3),
+(230,	4,	'17:00:00',	10,	1,	3),
+(231,	4,	'18:00:00',	10,	1,	3);
 
 DROP TABLE IF EXISTS `store`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `store` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -170,25 +152,14 @@ CREATE TABLE `store` (
   UNIQUE KEY `name_UNIQUE` (`name`),
   UNIQUE KEY `address_UNIQUE` (`address`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=188 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `store`
---
-
-LOCK TABLES `store` WRITE;
-/*!40000 ALTER TABLE `store` DISABLE KEYS */;
-/*!40000 ALTER TABLE `store` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ticket`
---
+INSERT INTO `store` (`id`, `name`, `address`, `max_capacity`, `curr_number`, `latitude`, `longitude`) VALUES
+(1,	'Store Aurora',	'Via Spinoza, 4, Milano',	100,	0,	45.4796,	9.22525),
+(2,	'Store Vincenza',	'Piazza Leonardo, 26, Milano',	60,	0,	45.4769,	9.22612),
+(3,	'Store Olga',	'Viale Romagna, 61, Milano ',	40,	0,	45.4786,	9.22335);
 
 DROP TABLE IF EXISTS `ticket`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ticket` (
   `type` enum('reservation','queue','priority') NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
@@ -207,25 +178,10 @@ CREATE TABLE `ticket` (
   CONSTRAINT `ticket_reservation` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`),
   CONSTRAINT `ticket_store` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`),
   CONSTRAINT `ticket_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=148 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `ticket`
---
-
-LOCK TABLES `ticket` WRITE;
-/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `token`
---
 
 DROP TABLE IF EXISTS `token`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `token` (
   `user_id` varchar(20) NOT NULL,
   `token` varchar(36) NOT NULL,
@@ -233,24 +189,11 @@ CREATE TABLE `token` (
   KEY `token_user_idx` (`user_id`),
   CONSTRAINT `token_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `token`
---
-
-LOCK TABLES `token` WRITE;
-/*!40000 ALTER TABLE `token` DISABLE KEYS */;
-/*!40000 ALTER TABLE `token` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
+INSERT INTO `token` (`user_id`, `token`, `end_timestamp`) VALUES
+('totem',	'totem-token',	'2025-01-01 00:00:00');
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` varchar(20) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -259,48 +202,17 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone_number_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `verification_code`
---
+INSERT INTO `user` (`id`, `name`, `surname`, `is_totem`) VALUES
+('totem',	'Totemo',	'De Totemis',	1);
 
 DROP TABLE IF EXISTS `verification_code`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `verification_code` (
   `number` varchar(20) NOT NULL,
   `code` char(5) DEFAULT NULL,
   PRIMARY KEY (`number`),
   CONSTRAINT `code_number` FOREIGN KEY (`number`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `verification_code`
---
 
-LOCK TABLES `verification_code` WRITE;
-/*!40000 ALTER TABLE `verification_code` DISABLE KEYS */;
-/*!40000 ALTER TABLE `verification_code` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-02-03 13:12:55
+-- 2021-02-06 17:56:07
--- ITD/database/tuples.sql
+++ ITD/database/tuples.sql
@@ -12,18 +12,79 @@ delete from token;
 delete from verification_code;
 
 insert into admin values (1, "admin", "password");
-insert into store values (1, "Store 1", "Piazza Leonardo 32, Milano", 100, 0, 0, 0);
+insert into store values (1, "Store Aurora", "Via Spinoza, 4, Milano", 100, 0, 45.47962, 9.22525);
+insert into store values (2, "Store Vincenza", "Piazza Leonardo, 26, Milano", 60, 0, 45.47690, 9.22612);
+insert into store values (3, "Store Olga", "Viale Romagna, 61, Milano ", 40, 0, 45.47862, 9.22335);
+
 insert into managed_store values (1, 1);
+insert into managed_store values (2, 1);
+insert into managed_store values (3, 1);
+
 insert into user values ('totem', 'Totemo', 'De Totemis', 1);
 insert into token values ('totem', 'totem-token', '2025-01-01 00:00:00');
 
-insert into reservation values (1, 1, "16:00", 10, TRUE, 1);
-insert into reservation values (2, 1, "16:30", 10, TRUE, 1);
-insert into reservation values (3, 1, "17:00", 10, TRUE, 1);
-insert into reservation values (4, 1, "17:30", 10, TRUE, 1);
-insert into reservation values (5, 1, "18:00", 10, TRUE, 1);
-insert into reservation values (6, 1, "18:30", 10, TRUE, 1);
-insert into reservation values (7, 1, "19:00", 10, TRUE, 1);
-insert into reservation values (8, 1, "19:30", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "08:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "10:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "13:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "16:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "17:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "18:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "08:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "10:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "16:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "17:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "18:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "08:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "10:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "16:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "18:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "08:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "10:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "16:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "17:00", 10, TRUE, 1);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "18:00", 10, TRUE, 1);
+
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "08:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "10:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "13:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "16:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "17:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "18:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "08:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "10:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "16:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "17:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "18:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "08:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "10:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "16:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "18:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "08:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "10:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "16:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "17:00", 10, TRUE, 2);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "18:00", 10, TRUE, 2);
+
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "08:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "10:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "13:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "16:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "17:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 1, "18:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "08:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "10:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "16:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "17:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 2, "18:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "08:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "10:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "16:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 3, "18:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "08:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "10:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "16:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "17:00", 10, TRUE, 3);
+insert into reservation(weekday,start_time, max_people_allowed, is_active, store_id)values ( 4, "18:00", 10, TRUE, 3);
+
 
 set FOREIGN_KEY_CHECKS = 1;
